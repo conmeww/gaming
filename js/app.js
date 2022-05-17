7a7237aab714275f0a32afd3f6503e720145b7b9
conmeww <73779105+conmeww@users.noreply.github.com>
2022-05-17 15:39:27 +0300

    commit

--- js/app.js
+++ js/app.js
@@ -3036,29 +3036,6 @@
     (() => {
         "use strict";
         const flsModules = {};
-        let isMobile = {
-            Android: function() {
-                return navigator.userAgent.match(/Android/i);
-            },
-            BlackBerry: function() {
-                return navigator.userAgent.match(/BlackBerry/i);
-            },
-            iOS: function() {
-                return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-            },
-            Opera: function() {
-                return navigator.userAgent.match(/Opera Mini/i);
-            },
-            Windows: function() {
-                return navigator.userAgent.match(/IEMobile/i);
-            },
-            any: function() {
-                return isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows();
-            }
-        };
-        function addTouchClass() {
-            if (isMobile.any()) document.documentElement.classList.add("touch");
-        }
         function addLoadedClass() {
             window.addEventListener("load", (function() {
                 setTimeout((function() {
@@ -11007,7 +10984,6 @@
             }));
         }));
         window["FLS"] = true;
-        addTouchClass();
         addLoadedClass();
         menuInit();
         spollers();
